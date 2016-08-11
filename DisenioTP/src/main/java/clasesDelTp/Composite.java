5c9e64e011d26c451bb373c4dc7cadb5556555e2
GuidoOliveri <guido.oli@hotmail.com>
2016-08-11 16:20:30 -0300

    una cosa que me olvide de incluir

--- DisenioTP/src/main/java/clasesDelTp/Composite.java
+++ DisenioTP/src/main/java/clasesDelTp/Composite.java
@@ -5,17 +5,12 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Set;
 
-//ver como agregar el import de arrays
-
 public class Composite implements Componente {
-	
+
 	private ArrayList<Componente> procesos = new ArrayList<Componente>();
 	
 	public void agregarProceso (Componente unProceso){
 		procesos.add(unProceso);
 	}
 	
-	void ejecutar(java.awt.event.ActionEvent hola){
-		//hola
-	}
 }
